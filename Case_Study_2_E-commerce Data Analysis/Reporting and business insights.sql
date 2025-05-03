fca7dfdbd5254ab782febe1b8fede2ad82efe3a2
Hasnaa Jinni <hassnajinni@gmail.com>
2025-05-03 14:37:14 +0100

    payment analysis

--- Case_Study_2_E-commerce Data Analysis/Reporting and business insights.sql
+++ Case_Study_2_E-commerce Data Analysis/Reporting and business insights.sql
@@ -32,3 +32,29 @@ SELECT
   (COUNT(CASE WHEN last_order_date <= DATE_SUB(CURRENT_DATE, INTERVAL 6 MONTH) THEN 1 END) / 
    COUNT(DISTINCT customer_id)) * 100 AS churn_rate
 FROM ChurnRateCTE;
+
+/*
+Monthly Payment Trend with Success/Failure Breakdown 
+*/
+SELECT
+  DATE_FORMAT(payment_date, '%Y-%m') AS month,
+  COUNT(*) AS total_payments,
+  SUM(CASE WHEN payment_status = 'completed' THEN 1 ELSE 0 END)          AS successful_payments,
+  SUM(CASE WHEN payment_status <> 'completed' THEN 1 ELSE 0 END)          AS failed_payments,
+  ROUND(
+    100.0 * SUM(CASE WHEN payment_status = 'completed' THEN 1 ELSE 0 END) 
+    / COUNT(*), 
+    2
+  ) AS success_rate_pct
+FROM payments
+GROUP BY month
+ORDER BY month;
+
+
+
+/*How much revenue is lost each month due to failed or declined payments,
+ and which payment methods contribute most to this loss
+*/
+
+select  P.payment_method, DATE_FORMAT(payment_date, '%Y-%m') AS month,Round(sum(O.total_amount),2) as lost_revenue from Payments P inner join orders O 
+ON P.order_id = O.order_id where P.payment_status = "Failed" group by P.payment_method, month order by month,lost_revenue desc;
